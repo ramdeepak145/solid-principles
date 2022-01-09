471bfd23a0ef12429f62dfd1d35a010c3b316579
ramdeepak145 <96425871+ramdeepak145@users.noreply.github.com>
2022-01-09 22:09:19 +0530

    Update Email.java

--- Email.java
+++ Email.java
@@ -1,5 +1,6 @@
 package com.solidprinciples;
 
+//Follows Interface Segregation here interface email depends on the method sendMail() it use.
 public interface Email
 {
     public String sendMail(UserDetails data);
